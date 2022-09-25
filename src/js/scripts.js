e03c0b7a283fedab2bed5b6ddcc1c184f6ba9916
alexneo68 <tunning_brain@mail.ru>
2022-09-25 18:28:29 +0300

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,61 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const dogUrl = new URL('../assets/doggo2.glb', import.meta.url)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background
+renderer.setClearColor(0xFEFEFE);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Sets orbit control to move the camera around
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// Camera positioning
+camera.position.set(6, 8, 14);
+orbit.update();
+
+// Sets a 12 by 12 gird helper
+const gridHelper = new THREE.GridHelper(12, 12);
+scene.add(gridHelper);
+
+// Sets the x, y, and z axes with each having a length of 4
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+const gltfLoader = new GLTFLoader();
+let mixer = null;
+gltfLoader.load(dogUrl.href, gltf => {
+    const model = gltf.scene
+    scene.add(model)
+    mixer = new THREE.AnimationMixer(model)
+    const clips = gltf.animations;
+    clips.forEach(clip => {
+        const action = mixer.clipAction(clip);
+        action.play()
+    });
+})
+const clock = new THREE.Clock()
+function animate() {
+    if (mixer) mixer.update(clock.getDelta())
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
